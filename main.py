8be3a01ce6a6c361aaed99c01cf850ae2fc65292
MrsIceWomen <vlad.nester.2013@gmail.com>
2024-07-29 18:18:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import random
 import logging
 
-
 class TreasureMap:
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, MAP_SIZE):
+        self.size = MAP_SIZE
         self.treasure_location = self.generate_treasure_location()
 
     def generate_treasure_location(self):
@@ -13,40 +12,40 @@ class TreasureMap:
         return x, y
 
     def is_treasure_at(self, x, y):
-        return (x, y) == self.treasure_location
+        return (x,y) == self.treasure_location
 
     def get_help(self, x, y):
         treasure_x, treasure_y = self.treasure_location
         distance = abs(treasure_x - x) + abs(treasure_y - y)
         if distance == 0:
-            return 'You found the treasure!!!'
+            return 'You found the treasure!'
         elif distance <= 1:
-            return 'Very hot!\n'
+            return 'Very hot'
         elif distance <= 3:
-            return 'Hot!\n'
+            return 'Hot'
         elif distance <= 5:
-            return 'Warm!\n'
+            return 'Warm'
         elif distance <= 7:
-            return 'Cold.\n'
+            return 'Cold'
         else:
-            return 'Very cold.\n'
+            return 'Very cold'
 
 
 class Player:
     def __init__(self):
-        self.attemps = 0
-        self.coordinates = []
+        self._attemps = 0
+        self._coordinates = []
 
     def choose_coordinates(self, x, y):
-        self.coordinates.append((x, y))
-        self.attemps += 1
-        print('Attempt №' + str(self.attemps))
+        self._coordinates.append((x, y))
+        self._attemps += 1
+        print('Attempt number: ' + str(self._attemps)) 
 
 
 class Game:
     def __init__(self):
         self.size = self.choose_size()
-        self.max_attemps = self.size + 5
+        self.__max_attemps = self.size + 5
         self.map = TreasureMap(self.size)
         self.player = Player()
         logging.basicConfig(filename='log.txt', level=logging.INFO)
@@ -54,33 +53,35 @@ class Game:
     def choose_size(self):
         while True:
             try:
-                size = int(input('Choose the size of the map: '))
-                if size > 4:
-                    return size
+                MAP_SIZE = int(input('Choose the size of the map: '))
+                if MAP_SIZE > 4:
+                    return MAP_SIZE
                 else:
                     raise ValueError('The size must be greater than 4')
             except ValueError:
                 print('Invalid input. Please enter a number greater than 4.')
 
+
     def start(self):
         print('Welcome to the TreasureMap game!')
         print(f'The map is {self.size}x{self.size}.')
-        print(f'You have {self.max_attemps} attempts to find the treasure.')
+        print(f'You have {self.__max_attemps} attempts to find the treasure.')
         print('You have to find the treasure by choosing coordinates.')
         print('Enter the coordinates one by one (first x, then y).')
         print('Good luck!\n')
 
-        while self.player.attemps < self.max_attemps:
+        while self.player._attemps < self.__max_attemps:
             try:
-                x = self.get_x()
-                y = self.get_y()
+                x = self.get_coordinate('x')
+                y = self.get_coordinate('y')
                 self.player.choose_coordinates(x, y)
-                print(f'You have {self.max_attemps - self.player.attemps} attemps.')
                 logging.info(f'Player chose coordinates: ({x}, {y}).')
                 if self.map.is_treasure_at(x, y):
                     print(self.map.get_help(x, y))
                     print('Congratulations! You win!')
-                    print(f'It took you {self.player.attemps} attempts.')
+                    print(f'It took you {self.player._attemps} attempts.')
+                    clue = self.map.get_help(x, y)
+                    logging.info(f'Clue: {clue}')
                     break
                 else:
                     clue = self.map.get_help(x, y)
@@ -88,29 +89,19 @@ class Game:
                     logging.info(f'Clue: {clue}')
             except ValueError as e:
                 print(f'Error: {e}')
-        if self.player.attemps >= self.max_attemps:
+        if self.player._attemps >= self.__max_attemps:
             print('You lose! You have used all your attempts.')
             logging.info('Player lost the game.')
 
-    def get_x(self):
+    def get_coordinate(self, number):
         while True:
             try:
-                x = int(input(f'Enter the x (1 to {self.size}): '))
-                if 0 < x <= self.size:
-                    return x
+                number = int(input(f'Enter the {number} (1 to {self.size}): '))
+                if 0 < number <= self.size:
+                    return number
                 else:
-                    raise ValueError(f'The x must be between 1 and {self.size}.')
-            except ValueError as e:
-                print(f'Error: {e}')
-
-    def get_y(self):
-        while True:
-            try:
-                y = int(input(f'Enter the y (1 to {self.size}): '))
-                if 0 < y <= self.size:
-                    return y
-                else:
-                    raise ValueError(f'The y must be between 1 and {self.size}.')
+                    number = 'coordinate'
+                    raise ValueError(f'The {number} must be between 1 and {self.size}.')
             except ValueError as e:
                 print(f'Error: {e}')
 
@@ -118,6 +109,7 @@ class Game:
 if __name__ == '__main__':
     game = Game()
     game.start()
+    
 
 
 
@@ -152,11 +144,11 @@ class TestPlayer(unittest.TestCase):
     def test_choose_coordinates(self):
         player = Player()
         player.choose_coordinates(1, 1)
-        self.assertEqual(player.coordinates, [(1, 1)])
-        self.assertEqual(player.attemps, 1)
+        self.assertEqual(player._coordinates, [(1, 1)])
+        self.assertEqual(player._attemps, 1)
         player.choose_coordinates(2, 2)
-        self.assertEqual(player.coordinates, [(1, 1), (2, 2)])
-        self.assertEqual(player.attemps, 2)
+        self.assertEqual(player._coordinates, [(1, 1), (2, 2)])
+        self.assertEqual(player._attemps, 2)
 
 
 class TestGame(unittest.TestCase):
@@ -167,3 +159,4 @@ class TestGame(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
